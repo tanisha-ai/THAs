c657046393fab895a19abb57fad279b7f9414d5f
tanisha-ai <59817991+tanisha-ai@users.noreply.github.com>
2021-07-11 10:34:27 +0530

    Add files via upload

---
+++ DAY-14/day14.js
@@ -0,0 +1,42 @@
+function clickme(){
+    console.log('yeah you clicked me');
+    
+}
+
+ function mouseover(){
+     console.log('your mouse is over me')
+ }
+ function mouseout(){
+     console.log('oohh now you are out of me')
+ }
+
+ function keyup(){
+     console.log('your key is up')
+
+ }
+
+ 
+ function keydown(){
+    console.log('your key is down')
+    
+}
+
+function focused(){
+    console.log('you are focusing on me')
+    
+}
+
+function blured(){
+    console.log('this is blurry')
+}
+
+function pageload(){
+console.log('page loaded successfully')
+}
+function offline(){
+    console.log('oohh i am offline')
+}
+
+function resizewindow(){
+    console.log('abey ku choti kr ra h')
+}
