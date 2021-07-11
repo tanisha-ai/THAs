b8f36f0dff0c2dc46eda297ebe1942c4afcd5fae
tanisha-ai <tanisha0103m@gmail.com>
2021-07-11 12:40:33 +0530

    THAs

---
+++ indexgenerator.py
@@ -0,0 +1,12 @@
+import os
+
+files = ["<h2 style='text-align:center;'><b> Devnest Take-Home Assignments </b></h2>\n", "<h4 style='text-align:center;'><b> Tanisha </b></h5>\n"]
+
+for dir, _, _ in os.walk("."):
+	if 'git' not in dir and not dir == ".":
+		name = dir.split("\\")
+		html_link = f"<a href='{dir}'>{name[-1]}</a><br>\n"
+		files.append(html_link)
+	
+with open("index.html", 'w+') as f:
+	f.writelines(files)
