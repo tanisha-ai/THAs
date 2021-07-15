ded8a3b6d26ffbdc2803be3f633f0dfacd046a46
tanisha-ai <59817991+tanisha-ai@users.noreply.github.com>
2021-07-15 22:49:43 +0530

    this is day-15

---
+++ day-15/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      hello world
+    </div>
+  );
+}
+
+export default App;
