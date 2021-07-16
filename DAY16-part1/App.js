0abb86993096a46f8b7658a0d3e398bf60795c36
tanisha-ai <59817991+tanisha-ai@users.noreply.github.com>
2021-07-16 20:16:58 +0530

    Add files via upload

---
+++ DAY16-part1/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Card() {
+ return(
+  <div className="meme" >
+    <img src="https://memesboy.com/wp-content/uploads/2018/03/When-A-Reccoon-Loves-Coconut-Meme.jpg" alt=""></img>
+    <h2>react world</h2>
+    <p>this is my first part of THA-16</p>
+  </div>
+ );
+}
+
+export default Card;
