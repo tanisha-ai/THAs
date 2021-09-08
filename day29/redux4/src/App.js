ecf6eb3504ba5092ea2fe83bcb92a7fe81e59ca2
tanisha-ai <59817991+tanisha-ai@users.noreply.github.com>
2021-09-08 20:49:38 +0530

    Add files via upload

---
+++ day29/redux4/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Form from './components/Form';
+import Card from './components/Card';
+
+function App() {
+  // const updatePlaceData=()=>{
+
+  // }
+  return (
+    <div className="App">
+      <Form/>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
